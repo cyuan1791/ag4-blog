9b2562f04d72f8dd8d66c95347b68803d144cb1d
cmsnow <cyuan123@live.com>
2017-06-03 20:47:27 -0700

    update

--- src/app/blog.service.ts
+++ src/app/blog.service.ts
@@ -1,11 +1,12 @@
+import {MyUtil} from './myutil';
 
 export class BlogService {
   blogData = {
     activeId: 0,
     data: [
-      {'title': 'title one', 'imgUrl': 'http://archive.webcmsd.com/images/bridge-1440x750.jpg'},
-      {'title': 'title two', 'imgUrl': 'http://archive.webcmsd.com/images/mountains1386x554.jpg'},
-      {'title': 'title three', 'imgUrl': 'http://archive.webcmsd.com/images/sf_gate1440x350.jpg'},
+      {'title': 'title one', 'imgUrl': 'http://archive.webcmsd.com/images/bridge-1440x750.jpg', 'desc': MyUtil.text()},
+      {'title': 'title two', 'imgUrl': 'http://archive.webcmsd.com/images/mountains1386x554.jpg', 'desc': MyUtil.text()},
+      {'title': 'title three', 'imgUrl': 'http://archive.webcmsd.com/images/sf_gate1440x350.jpg', 'desc': MyUtil.text()},
     ]
   }
 
